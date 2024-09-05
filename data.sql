462c5db74f64e1c79b2684e3024aa53271658f0b
MELB26 <90427804+MELB26@users.noreply.github.com>
2024-09-05 21:35:21 +0800

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,166 @@
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (1, 'john05', 'Darren', 'Fox', '1976-06-15', '#$0n3RGsZS', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (2, 'burtonlance', 'Alicia', 'Lyons', '1965-03-30', '7+8P6Ktv4f', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (3, 'choirichard', 'Mark', 'Farmer', '1975-05-14', '+NtKLS*j3S', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (4, 'christopher85', 'Shawn', 'Davis', '1957-09-26', '86wP_FRg@i', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (5, 'walkernathan', 'Brandon', 'White', '1962-09-29', 'gU6F+cRYg^', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (6, 'colesoto', 'Jennifer', 'Rivers', '1945-11-13', '$97#4Zjkrd', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (7, 'romerograce', 'Andrew', 'Peck', '2001-08-26', 'k05X92fU%d', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (8, 'adambass', 'Amanda', 'Gonzalez', '2004-03-06', '$2Y0hk1C9C', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (9, 'swebster', 'Autumn', 'Harding', '1971-02-14', '+kb9tPjfq6', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (10, 'jwilson', 'Michael', 'Hernandez', '1982-03-08', '(ZDUza_Ja0', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (11, 'qdavis', 'Lori', 'Leblanc', '1966-10-25', '#leWhQfyT8', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (12, 'mannmonica', 'Devon', 'James', '1992-04-30', 'mV5QNMrs&M', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (13, 'vjacobs', 'William', 'Love', '1949-03-03', '1U%0QJKf(c', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (14, 'lawrenceperez', 'Brandon', 'Palmer', '1968-03-20', '#IBJluwD6h', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (15, 'idouglas', 'Willie', 'Miller', '1995-06-02', '7_Fqe$QP_)', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (16, 'elizabeth46', 'Austin', 'Torres', '1996-11-29', '_wWIuZ6m*8', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (17, 'nguyenian', 'Logan', 'Rodriguez', '1945-10-23', 'd@5HbLcw22', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (18, 'anitaroy', 'Jessica', 'Solis', '1967-07-07', '&J@N9BzO8#', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (19, 'barry12', 'William', 'Kelly', '1993-02-26', '$RxWRdoIm6', '2024-09-05 12:18:31');
+INSERT INTO Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) 
+VALUES (20, 'robin08', 'Jacob', 'Blackburn', '1973-06-19', 'L1#9xJ_rUN', '2024-09-05 12:18:31');
+
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (1, 2, 3, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (2, 3, 4, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (3, 4, 5, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (4, 5, 6, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (5, 6, 7, True, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (6, 7, 8, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (7, 8, 9, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (8, 9, 10, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (9, 10, 11, True, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (10, 11, 12, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (11, 12, 13, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (12, 13, 14, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (13, 14, 15, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (14, 15, 16, True, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (15, 16, 17, True, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (16, 17, 18, False, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (17, 18, 19, True, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (18, 19, 20, True, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (19, 20, 1, True, '2024-09-05 12:25:41');
+INSERT INTO Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded)
+VALUES (20, 1, 2, True, '2024-09-05 12:25:41');
+
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (1, 'Travel Buddies', 1004, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (2, 'Movie Buffs', 1011, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (3, 'Adventure Seekers', 1001, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (4, 'Tech Enthusiasts', 1016, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (5, 'Coffee Addicts', 1001, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (6, 'Gaming Squad', 1001, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (7, 'Nature Lovers', 1006, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (8, 'Music Lovers', 1016, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (9, 'Photography Lovers', 1013, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (10, 'Movie Buffs', 1018, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (11, 'Art Admirers', 1001, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (12, 'Foodies', 1013, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (13, 'Pet Lovers', 1019, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (14, 'Coffee Addicts', 1002, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (15, 'Nature Lovers', 1015, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (16, 'Adventure Seekers', 1015, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (17, 'Music Lovers', 1003, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (18, 'Adventure Seekers', 1005, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (19, 'Music Lovers', 1002, '2024-09-05 12:28:49');
+INSERT INTO Groups (GroupID, GroupName, CreatedBy, DateAdded)
+VALUES (20, 'Car Enthusiasts', 1008, '2024-09-05 12:28:49');
+
+INSERT INTO GroupMembershipRequests (GroupMemberShipRequestsID, GroupID, GroupMemberUserID, IsGroupMemberShipAccepted, DateAccepted) VALUES
+(1, 1, 1, TRUE, '2024-09-01 10:15:00'),
+(2, 2, 2, FALSE, NULL),
+(3, 3, 3, TRUE, '2024-09-02 14:25:00'),
+(4, 4, 4, FALSE, NULL),
+(5, 5, 5, TRUE, '2024-09-03 16:30:00'),
+(6, 6, 6, TRUE, '2024-09-04 11:00:00'),
+(7, 7, 7, FALSE, NULL),
+(8, 8, 8, TRUE, '2024-09-05 09:45:00'),
+(9, 9, 9, TRUE, '2024-09-06 13:20:00'),
+(10, 10, 10, FALSE, NULL),
+(11, 1, 11, TRUE, '2024-09-07 15:55:00'),
+(12, 2, 12, FALSE, NULL),
+(13, 3, 13, TRUE, '2024-09-08 17:10:00'),
+(14, 4, 14, TRUE, '2024-09-09 18:25:00'),
+(15, 5, 15, FALSE, NULL),
+(16, 6, 16, TRUE, '2024-09-10 12:00:00'),
+(17, 7, 17, TRUE, '2024-09-11 14:45:00'),
+(18, 8, 18, FALSE, NULL),
+(19, 9, 19, TRUE, '2024-09-12 16:10:00'),
+(20, 10, 20, FALSE, NULL);
+
+INSERT INTO Posts (PostID, PostDescription, PostedBy, IsPublic, IsOnlyForFriends, GroupID) VALUES
+(1, 'Excited about the new project launch!', 1, TRUE, FALSE, 3),
+(2, 'Had a great day at the park with friends.', 2, TRUE, TRUE, 5),
+(3, 'Check out this awesome article I found!', 3, TRUE, FALSE, 2),
+(4, 'Finished reading an amazing book today.', 4, FALSE, TRUE, 1),
+(5, 'Just had the best coffee ever.', 5, TRUE, FALSE, 4),
+(6, 'Looking forward to the weekend trip!', 6, TRUE, TRUE, 6),
+(7, 'Help needed for a group assignment.', 7, FALSE, TRUE, 2),
+(8, 'Had a productive meeting this morning.', 8, TRUE, FALSE, 3),
+(9, 'The weather is perfect for a run!', 9, TRUE, TRUE, 5),
+(10, 'Just saw a great movie last night.', 10, TRUE, FALSE, 4),
+(11, 'Can anyone recommend a good restaurant?', 11, TRUE, FALSE, 7),
+(12, 'Enjoying a relaxing day off.', 12, FALSE, TRUE, 1),
+(13, 'Just got a new phone, love it!', 13, TRUE, FALSE, 4),
+(14, 'Looking for new music recommendations.', 14, TRUE, TRUE, 2),
+(15, 'Had a fantastic dinner with family.', 15, FALSE, TRUE, 6),
+(16, 'Got some amazing feedback on my project.', 16, TRUE, FALSE, 3),
+(17, 'Trying out a new recipe tonight.', 17, TRUE, TRUE, 5),
+(18, 'Excited for the concert this weekend.', 18, TRUE, FALSE, 7),
+(19, 'Just finished a big assignment.', 19, FALSE, TRUE, 1),
+(20, 'Planning a game night with friends.', 20, TRUE, TRUE, 4);
